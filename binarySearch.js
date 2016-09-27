df4b4bed6efab5d5a7df38d9cfcb0e889496ff41
ecullu <emrecullu@msn.com>
2016-09-27 18:38:56 -0500

    fixed log(n) runtime

--- binarySearch.js
+++ binarySearch.js
@@ -38,8 +38,11 @@ assert(findInt3(intArr,6) === true)
 assert(findInt3(intArr,17) !== true)
 
 
-//log(n) runtime
-let intArr2 = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+//(n) runtime
+let intArr2 = []
+for (let i = 1; i < 1000; i++){
+    intArr2.push(i)
+}
 let findInt4 = function (intArr2,num){
     let midIndex = parseInt(intArr2.length/2)
     
@@ -59,4 +62,51 @@ let findInt4 = function (intArr2,num){
     }
 }
 assert(findInt4(intArr2,4) === true)
-assert(findInt4(intArr2,14) !== true)
+assert(findInt4(intArr2,1001) !== true)
+
+//log(n) runtime
+let findInt5 = function (intArr2,num,start,end,exMid){
+    var exMid = exMid
+    if (start !== undefined && end !== undefined){
+        if ((end - start) > 1){
+            var midIndex = end - parseInt((end-start)/2)
+        }
+        else if ((end - start) === 1 && end === (intArr2.length-1)){
+            var midIndex = end
+        }
+        else if ((end - start) === 1 && start === 0){
+            var midIndex = 0
+        }
+        else{
+            var midIndex = start + parseInt((end-start)/2)
+        }
+    }
+    else{
+        var midIndex = parseInt(intArr2.length/2)
+    }
+    
+    if ( (end-start) === 1 && num !== intArr2[midIndex]){
+        return false
+        }
+    if (num === intArr2[midIndex]){
+        return true
+    }
+    else if (num < intArr2[midIndex]){
+        if (num > intArr2[exMid]){
+            return findInt5(intArr2,num,exMid,midIndex,midIndex)
+        }
+        else{
+            return findInt5(intArr2,num,0,midIndex, midIndex)
+        }
+    }
+    else{
+        if (num > intArr2[midIndex] && end === undefined){
+            return findInt5(intArr2,num,midIndex,(intArr2.length-1), midIndex)
+        }
+        else{
+            return findInt5(intArr2,num,midIndex,end)
+        }
+    }
+}
+assert(findInt5(intArr2,4) === true)
+assert(findInt5(intArr2,1001) !== true)
